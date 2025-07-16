3ddab5ba9363a067c6dd4b6f1c3227cfc07790a2
Wallace Oliveira <wallacebatistaoliveira@gmail.com>
2025-07-16 16:40:35 -0300

    feat(tasks): add get tasks by email and date interval

--- src/main/java/dev/iamwallace/tasks/infrastructure/repository/TaskRepository.java
+++ src/main/java/dev/iamwallace/tasks/infrastructure/repository/TaskRepository.java
@@ -4,7 +4,11 @@ import dev.iamwallace.tasks.infrastructure.entity.TaskEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends MongoRepository<TaskEntity, String> {
-
+  List<TaskEntity> findByEventDateBetween(LocalDateTime start, LocalDateTime end);
+  List<TaskEntity> findByEmail(String email);
 }
